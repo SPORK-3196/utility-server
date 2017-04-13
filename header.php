2b95108dc0f068ad79b3161ccf44ee24e764c323
Justin Stephens <jstephens2020@outlook.com>
2017-04-13 15:55:27 -0400

    Moved the batterypage from Index.php to batteries.php
    Also implemented a PHP-inserted header that can be included on all pages

--- header.php
+++ header.php
@@ -1,7 +1,18 @@
-<?php echo <<< HEADER_EOF
+<?php
+echo <<< HEADER_END
 
 <div id="header">
-	<h1> TEST HEADER </h1>
+	<ul id="navmenu">
+		<li><a href="index.php">Home</a></li>
+		<li><a href="batteries.php">Batteries</a></li>
+		<li class="dropdown"><a href="#" class="dropbtn">Dropdown Test</a>
+			<div>
+				<a href="#">Link 1</a>
+				<a href="#">Link 2</a>
+			</div>
+		</li>
+	</ul>
 </div>
 
-HEADER_EOF; ?>
+HEADER_END
+?>
--- index.php
+++ index.php
@@ -1,51 +1,17 @@
 <!DOCTYPE html>
-<?php
 
-	include("connect.php"); 	
-	
-	$link=Connection();
 
-	$result=mysql_query("SELECT * FROM `BatteryData` ORDER BY `entryDate` DESC",$link);
-?>
 
 <html>
 <head>
 	<title>Battery Log</title>
-	<link rel="stylesheet" type="text/css" href="/theme.css" />
+	<link rel="stylesheet" type="text/css" href="theme.css" />
+	<link rel="stylesheet" type="text/css" href="header.css" />
 </head>
 
 
 
 <body>
-<?php include("/header.php"); ?>
-
-   <h1>Battery Log</h1>
-
-   <table class="data">
-		<tr>
-			<td>&nbsp;Timestamp&nbsp;</td>
-			<td>&nbsp;Class&nbsp;</td>
-			<td>&nbsp;ID&nbsp;</td>
-			<td>&nbsp;Status&nbsp;</td>
-			<td>&nbsp;Charge&nbsp;</td>
-			<td>&nbsp;V0&nbsp;</td>
-			<td>&nbsp;V1&nbsp;</td>
-			<td>&nbsp;V2&nbsp;</td>
-			<td>&nbsp;RInt&nbsp;</td>
-			<td>&nbsp;Event&nbsp;</td>
-		</tr>
-
-      <?php 
-		  if($result!==FALSE){
-		     while($row = mysql_fetch_array($result)) {
-		        printf("<tr><td> &nbsp;%s </td><td> &nbsp;%s&nbsp; </td><td> &nbsp;%s&nbsp; </td><td> &nbsp;%s&nbsp; </td><td> &nbsp;%s&nbsp; </td><td> &nbsp;%s&nbsp; </td><td> &nbsp;%s&nbsp; </td><td> &nbsp;%s&nbsp; </td><td> &nbsp;%s&nbsp; </td><td> &nbsp;%s&nbsp; </td></tr>", 
-		           $row["entryDate"], $row["class"], $row["id"], $row["status"], $row["charge"], $row["v0"], $row["v1"], $row["v2"], $row["rint"], $row["event"]);
-		     }
-		     mysql_free_result($result);
-		     mysql_close();
-		  }
-      ?>
-
-   </table>
+	<?php include("header.php"); ?>
 </body>
 </html>
