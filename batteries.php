739d61b1c4edf4888182dcf83aadadf5e9f0940b
Justin Stephens <jstephens2020@outlook.com>
2017-04-13 20:40:07 -0400

    Adds a form on the battery page to allow for inputing data via computer.
    Also adds several animations to the header, dropdown menu, and the form box to make the page more lively.

--- batteries.php
+++ batteries.php
@@ -19,9 +19,37 @@
 
 <body>
 <?php include("header.php"); ?>
-   <h1>Battery Log</h1>
+   <div id="formbox">
+      <h1>Add Entry</h1>
+      <form action="add.php" method="get">
+         Class: <input type="text" name="class" maxlength="1">
+         ID: <input type="number" name="id" min="0" max="15"> <br>
+         Status: <input type="radio" name="status" value=0> Good
+              <input type="radio" name="status" value=1> Needs Charge <br>
+         Charge: <input type="number" name="charge" min="0" max="255"> <br>
+         V0: <input type="number" name="v0" min="0" max="16.384" step="0.001"> <br>
+         V1: <input type="number" name="v1" min="0" max="16.384" step="0.001"> <br>
+         V2: <input type="number" name="v2" min="0" max="16.384" step="0.001"> <br>
+         RInt: <input type="number" name="rint" min="0" max="0.001" step="0.001"> <br>
+         <input type="submit">
+         <select name="event" form="event">
+            <option value=0>On charger</option>
+            <option value=1>Off charger</option>
+            <option value=2>On robot</option>
+            <option value=3>Off robot</option>
+            <option value=4>CHG->RBT</option>
+            <option value=5>RBT->CHG</option>
+            <option value=6>CHG->STR</option>
+            <option value=7>STR->CHG</option>
+            <option value=8>Checkup</option>
+         </select>
+      </form>
+   </div>
 
-   <table class="data">
+   <div id="tablebody">
+      <h1>Battery Log</h1>
+
+      <table class="data">
 		<tr>
 			<td>&nbsp;Timestamp&nbsp;</td>
 			<td>&nbsp;Class&nbsp;</td>
@@ -35,7 +63,7 @@
 			<td>&nbsp;Event&nbsp;</td>
 		</tr>
 
-      <?php 
+         <?php 
 		  if($result!==FALSE){
 		     while($row = mysql_fetch_array($result)) {
 		        printf("<tr><td> &nbsp;%s </td><td> &nbsp;%s&nbsp; </td><td> &nbsp;%s&nbsp; </td><td> &nbsp;%s&nbsp; </td><td> &nbsp;%s&nbsp; </td><td> &nbsp;%s&nbsp; </td><td> &nbsp;%s&nbsp; </td><td> &nbsp;%s&nbsp; </td><td> &nbsp;%s&nbsp; </td><td> &nbsp;%s&nbsp; </td></tr>", 
@@ -44,8 +72,10 @@
 		     mysql_free_result($result);
 		     mysql_close();
 		  }
-      ?>
+         ?>
 
-   </table>
+      </table>
+   </div>
+
 </body>
 </html>
