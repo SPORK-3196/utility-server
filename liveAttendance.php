e334d1d39b8871a4ef0b9aaf19b5d47377708c29
Justin Stephens <jstephens2020@outlook.com>
2017-12-18 20:28:31 -0500

    Updated the live attendance code to make use of the new util interface.

--- liveAttendance.php
+++ liveAttendance.php
@@ -1,40 +1,51 @@
 <!DOCTYPE html>
 <html>
-<meta http-equiv="refresh" content="1; URL=liveAttendance.php">
+<meta http-equiv="refresh" content="2; URL=liveAttendance.php">
 <head>
-	<title>Attendance Log</title>
+	<title>In shop?</title>
 	<link rel="stylesheet" type="text/css" href="theme.css" />
 	<link rel="stylesheet" type="text/css" href="header.css" />
 </head>
 
 <body>
-   <div id="tablebody">
-      <h1>Live Attendance</h1>
+	<div id="tablebody">
+		<h1>Who's in the shop?</h1>
 
-      <table class="data">
-		<tr>
-			<td>&nbsp;First Name&nbsp;</td>
-			<td>&nbsp;Last Name&nbsp;</td>
-		</tr>
+		<?php
+			// Creates as SQL connection, queries for all member IDs
+			include("util.php");
+			$sql = SQL::get_connection();
+			$idList = $sql->query("SELECT id FROM members");
 
-         <?php
-		include("connect.php");
 
-		$link = Connection();
+			// If mysqli->query() returned false, there was an invalid query
+			if ($idList === FALSE)
+				die ("<p class=\"error\">Internal query error</p>");
+			
+			// If no users are found, leave an error
+			else if ($idList->num_rows === 0)
+				die ("<p class=\"error\">No users in database</p>");
+		?>
 
-		$mResults = mysql_query("SELECT * FROM members");
+    	<table class="data">
+			<tr>
+				<td>Name</td>
+				<td>Time in</td>
+			</tr>
 
-		if($mResults == FALSE) { die("Error"); }
-
-		while($mRow = mysql_fetch_array($mResults)) {
-			if($mRow["inShop"] == "1" && !($mRow["fName"] == "Master" && $mRow["lName"] == "Key")) {
-				echo "<tr> <td>&nbsp;$mRow[fName]&nbsp;</td> <td>&nbsp;$mRow[lName]&nbsp;</td> </tr>";
-			}
-		}
-         ?>
-
-      </table>
-   </div>
-
+			<?php
+				// Loops through all IDs and loads members, outputing them to the table
+				// if they are signed in.
+				while($memberID = $idList->fetch_assoc()['id'])
+				{
+					$member = Member::SQL_Load_Member_ID($memberID);
+					$member->Refresh_Sign_Status();
+					if($member->signed_in) {
+						echo "<tr> <td>$member->firstName</td> <td>$member->lastName</td> </tr>";
+					}
+				}
+			?>
+		</table>
+	</div>
 </body>
 </html>
