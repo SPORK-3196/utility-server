82e009ed92c49bf18d44dcf9141890770acb9aae
Justin Stephens <jstephens2020@outlook.com>
2018-01-22 16:41:19 -0500

    Fixed error/bug where no names were outputted

--- liveAttendance.php
+++ liveAttendance.php
@@ -12,15 +12,6 @@
 	include("util.php");
 	$sql = SQL::get_connection();
 	$idList = $sql->query("SELECT id FROM members");
-
-
-	// If mysqli->query() returned false, there was an invalid query
-	if ($idList === false)
-		die ("<p class=\"error\">Internal query error</p>");
-	
-	// If no users are found, leave an error
-	else if ($idList->num_rows === 0)
-		die ("<p class=\"error\">No users in database</p>");
 ?>
 
 <body>
@@ -65,6 +56,16 @@
 	<div id="tablebody">
 		<h1>Who's in the shop?</h1>
 
+		<?php
+		// If mysqli->query() returned false, there was an invalid query
+		if ($idList === false)
+			die ("<p class=\"error\">Internal query error</p>");
+	
+		// If no users are found, leave an error
+		else if ($idList->num_rows === 0)
+			die ("<p class=\"error\">No users in database</p>");
+		?>
+
     	<table class="data">
 			<tr>
 				<td>Name</td>
@@ -74,13 +75,21 @@
 			<?php
 				// Loops through all IDs and loads members, outputing them to the table
 				// if they are signed in.
-				while($memberID = $idList->fetch_assoc()['id'])
+				while($memberRow = $idList->fetch_assoc())
 				{
-					$member = Member::SQL_Load_Member_ID($memberID);
-					$member->Refresh_Sign_Status();
-					if($member->signed_in == 1) {
-						echo "<tr> <td>$member->firstName</td> <td>$member->lastName</td> </tr>";
+					// Loads the member according to the id
+					$member = Member::SQL_Load_Member_ID($memberRow['id']);
+
+					// Display error if member couldn't be loaded
+					if ($member === false) {
+						echo "<tr> <td><p class=\"error\">Member not found</p></td> <td></td> </tr>";
+						continue;
 					}
+
+					// Refresh sign-in status and output name is present
+					$member->Refresh_Sign_Status();
+					if($member->signed_in == true)
+						echo "<tr> <td>$member->firstName</td> <td>$member->lastName</td> </tr>";
 				}
 			?>
 		</table>
